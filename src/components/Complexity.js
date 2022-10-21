af82f7244ed80e6efcecbe624eda02ac8bc2083e
Alexander K <jonhy.blaze@gmail.com>
2022-10-22 01:31:07 +0300

    adding <Complexity /> component logic

--- src/components/Complexity.js
+++ src/components/Complexity.js
@@ -1,28 +1,30 @@
 import React from "react";
 
 export default function Complexity(props) {
-  
 
   function complexityLevel () {
     if (props.length < 6) {
-      return 'This is silly'
+      return 'Silly'
     }
     if(props.length > 5 && props.length < 9) {
-      return 'This is a piece of cake'
+      return 'Poor'
     }
     if(props.length >= 9 && props.length < 13) {
-      return 'This is a gift for rascals'
+      return 'Compromised'
     }
     if(props.length >= 13 && props.length < 16) {
-      return 'This is descent'
+      return 'Descent'
     }
     if(props.length >= 16 && props.length < 18) {
-      return 'It\'s almost safe'
+      return 'Safe'
     }
     if(props.length >= 18 && props.length < 20) {
-      return 'You are secured'
+      return 'Secured'
     }
-    if(props.length >= 20 && props.length < 25) {
+    if(props.length >= 20 && props.length < 22) {
+      return 'Bulletproofed'
+    }
+    if(props.length >= 22 && props.length < 25) {
       return 'Paranoid'
     }
 
@@ -34,50 +36,69 @@ export default function Complexity(props) {
     if (props.length < 3) {
       return <span>It would take <strong>no time</strong> to crack it</span>
     }
-    if (props.length >= 3 && props.length < 5) {
+    if (props.length >= 3 && props.length < 4) {
       return <span>It would take <strong>split second</strong> to crack it</span>
     }
-    if(props.length >= 4 && props.length < 5) {
+    if(props.length === 4) {
       return <span>It would take <strong>less than 1 second</strong> to crack it</span>
+    }
+    if(props.length === 5) {
+      return <span>It would take <strong>few seconds</strong> to crack it</span>
     }
     if(props.length >= 5 && props.length < 7) {
       return <span>It would take <strong>less than minute</strong> to crack it</span>
     }
-    if(props.length >= 7 && props.length < 9) {
+    if(props.length === 7) {
       return <span>It would take <strong>less than an hour</strong> to crack it</span>
     }
-    if(props.length >= 9 && props.length < 12) {
+    if(props.length === 8) {
       return <span>It would take <strong>few hours</strong> to crack it</span>
     }
-    if(props.length >= 12 && props.length < 13) {
+
+    if(props.length === 9) {
       return <span>It would take <strong>a busy day</strong> to crack it</span>
     }
-    if(props.length >= 13 && props.length < 14) {
+    if(props.length === 10) {
       return <span>It would take <strong>a week</strong> to crack it</span>
     }
-   if(props.length >= 14 && props.length < 15) {
+    if(props.length === 11) {
+      return <span>It would take <strong>a month</strong> to crack it</span>
+    }
+   if(props.length === 12) {
       return <span>It would take <strong>few months</strong> to crack it</span>
     }
-    if(props.length >= 15 && props.length < 16) {
+    if(props.length === 13) {
       return <span>It would take <strong>about a year</strong> to crack it</span>
     }
-    if(props.length >= 16 && props.length < 17) {
-      return <span>It would take <strong>10 years</strong> to crack it</span>
+    if(props.length === 14) {
+      return <span>It would take <strong>couple of years</strong> to crack it</span>
     }
-    if(props.length >= 17 && props.length < 18) {
+    if(props.length === 15) {
+      return <span>It would take <strong>15 years</strong> to crack it</span>
+    }
+    if(props.length === 16) {
       return <span>It would take <strong>a century</strong> to crack it</span>
     }
-    if(props.length >= 18 && props.length < 19) {
-      return <span>It would take <strong>a thousands years</strong> to crack it</span>
+    if(props.length === 17) {
+      return <span>It would take <strong>thousands of years</strong> to crack it</span>
     }
-    if(props.length >= 19 && props.length < 20) {
-      return <span>It would take <strong>a millions of years</strong> to crack it</span>
+    if(props.length === 18) {
+      return <span>It would take <strong>a million years</strong> to crack it</span>
     }
-    if(props.length >= 20 && props.length < 22) {
+    if(props.length === 19) {
+      return <span>It would take <strong>100 million years</strong> to crack it</span>
+    }
+    if(props.length === 20) {
+      return <span>It would take <strong>a billion years</strong> to crack it</span>
+    }
+    if(props.length === 21) {
       return <span>It would take <strong>as long as planet exists</strong> to crack it</span>
     }
-    if(props.length >= 22 && props.length < 24) {
+    if(props.length === 22) {
       return <span>It would take <strong>as long as a sun shines</strong> to crack it</span>
+    }
+    if(props.length === 23) {
+      return <span>It would take <strong>as long as universe expands</strong> to crack it</span>
     }
     if(props.length >= 24) {
       return <span>It would take <strong>forever</strong> to crack it</span>
