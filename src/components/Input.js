5b56706817b43a454ec933dff3da444ca230bfb2
Alexander K <jonhy.blaze@gmail.com>
2022-10-24 16:48:46 +0300

    making number input read only

--- src/components/Input.js
+++ src/components/Input.js
@@ -15,6 +15,7 @@ export default function Input (props) {
                name="number"
                onChange={props.handleChange}
                value={props.length}
+               readOnly={true}
                />
         </label>
         <input type="range" 
